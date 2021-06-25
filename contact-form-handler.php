e331da18ec37ebf020192447c299ea6b25793b58
Vyom Gupta <64950836+vyomguptaa@users.noreply.github.com>
2021-06-25 20:17:57 +0530

    Add files via upload

---
+++ contact-form-handler.php
@@ -0,0 +1,21 @@
+<?php
+     $name=$_POST['name'];
+     $visitor_email=$_POST['email'];
+     $tel=$_POST['tel'];
+     $message=$_POST['message'];
+     
+     $email_from="vyomgupta31@gmail.com"
+     $email_subject="New Form Submission"
+     $email_body="Username: $name.\n".
+                    "User Email: $visitor_email.\n".
+                    "User Phone: $tel.\n".
+                    "User Message: $message.\n";
+
+    $to="vyomgupta31@gmail.com";
+    $headers="From: $email_from \r\n";
+    $headers ="Reply-To: $visitor_email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: index.html");
+
+?>
+   
